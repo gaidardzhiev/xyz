ace07958b8a916088f16ad711ee4fd4a91ba72e1
gaidardzhiev <1v4n@protonmail.com>
2025-08-30 18:42:56 +0000

    Sat Aug 30 06:42:56 PM UTC 2025

---
+++ make.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+[ command -v pdflatex 2>/dev/null ] || {
+	printf "no pdflatex...\n";
+	exit 1;
+}
+
+flatex() {
+	printf "do you want to build the LaTeX document form source? (y/n)\n"
+	read X
+	case "$X" in
+		[y]*)
+			printf "building LaTeX from source...\n"
+			pdflatex latex.tex || return 8
+			cp latex.pdf "$HOME" || return 16
+			;;
+		[n]*)
+			printf "you chose NOT to build LaTeX from source...\n"
+			;;
+		*)
+			printf "invalid response...\n"
+			printf "(yes/no)\n"
+			;;
+	esac
+}
+
+{ flatex; RET=$?; } || exit 1
+
+[ "$RET" -eq 0 ] 2>/dev/null || printf "%s\n" "$RET"
