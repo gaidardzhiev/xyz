d378b74337aedf2f7dbbbb5ac742d2090d2bdfbc
gaidardzhiev <1v4n@protonmail.com>
2025-10-24 05:14:38 +0000

    fix: quote variables to prevent word splitting and globbing

--- make.sh
+++ make.sh
@@ -8,11 +8,11 @@
 flatex() {
 	printf "do you want to build the LaTeX document form source? (y/n)\n"
 	read X
-	case "$X" in
+	case "${X}" in
 		[y]*)
 			printf "building LaTeX from source...\n"
 			pdflatex latex.tex || return 8
-			cp latex.pdf "$HOME" || return 16
+			cp latex.pdf "${HOME}" || return 16
 			;;
 		[n]*)
 			printf "you chose NOT to build LaTeX from source...\n"
@@ -24,6 +24,6 @@ flatex() {
 	esac
 }
 
-{ flatex; RET=$?; } || exit 1
+{ flatex; RET="${?}"; } || exit 1
 
-[ "$RET" -eq 0 ] 2>/dev/null || printf "%s\n" "$RET"
+[ "${RET}" -eq 0 ] 2>/dev/null || printf "%s\n" "${RET}"
